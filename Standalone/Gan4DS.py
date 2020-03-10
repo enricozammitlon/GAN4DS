a964203050e26bf90823cbbf87392c85b4f2c534
Enrico Zammit <enrico.zammitl@gmail.com>
2020-03-10 17:51:28 +0000

    Chenged checks

--- Standalone/Gan4DS.py
+++ Standalone/Gan4DS.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 from os.path import exists
 
 variables_of_interest=['s1']
-energies=['1','10','20','40','60']
+energies=map(str, range(5,70))
 unsen_energies=[]
 
 HP_NODES = hp.HParam('num_nodes', hp.Discrete([40, 50]))
--- Standalone/check_if_done.sh
+++ Standalone/check_if_done.sh
@@ -1,8 +1,14 @@
-stat = "$(tail -1 gan4ds.out)"
-compare = "--JOB DONE--"
-if [ $stat == $compare ]; then
-  echo "Job is done!"
-else
-    echo ERROR: Job is not done yet. 1>&2
-    exit 1 # terminate and indicate error
+FILE=gan4ds.out
+if [[ -f "$FILE" ]]; then
+  stat = "$(tail -1 gan4ds.out)"
+  compare = "--JOB DONE--"
+  if [ $stat == $compare ]; then
+    echo "Job is done!"
+  else
+      echo ERROR: Job is not ready yet. 1>&2
+      exit 1 # terminate and indicate error
+  fi
+  else
+      echo ERROR: Job is not started yet. 1>&2
+      exit 1 # terminate and indicate error
 fi
