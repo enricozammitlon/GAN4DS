78fc80d5d95d2d68c8184f27108a784dc53d1796
ricozl <ricozl@hepgpu5.blackett.manchester.ac.uk>
2020-03-21 23:40:09 +0000

    [DEPLOY]S1,S2,f200

--- Standalone/check_if_done.sh
+++ Standalone/check_if_done.sh
@@ -1,9 +1,10 @@
 FILE=gan4ds.out
 if [[ -f "$FILE" ]]; then
-  stat = "$(tail -1 gan4ds.out)"
-  compare = "--JOB DONE--"
+  stat="$(tail -1 gan4ds.out)"
+  compare="--JOB DONE--"
   if [ $stat == $compare ]; then
     echo "Job is done!"
+    echo -e "Email sent!" > gan4ds.out
   else
       echo ERROR: Job is not ready yet. 1>&2
       exit 1 # terminate and indicate error
