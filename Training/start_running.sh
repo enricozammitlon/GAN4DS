2a8f8678fd929b7d40d295a0d6ebde8a27aa52ef
jethwak <jethwak@hepgpu4.blackett.manchester.ac.uk>
2020-05-30 18:24:26 +0100

    Changed error in bash script

--- Training/start_running.sh
+++ Training/start_running.sh
@@ -2,4 +2,4 @@
 mkdir out/
 export SINGULARITY_DISABLE_CACHE=True
 singularity pull docker://enricozl/gan4ds:argan-runner
-nohup singularity run --nv --bind $(pwd)/out:out/ gan4ds_argan-runner.sif > gan4ds.out 2> gan4ds.err < /dev/null &
+nohup singularity run --nv --bind $(pwd)/out gan4ds_argan-runner.sif > gan4ds.out 2> gan4ds.err < /dev/null &
