57bd722c27b3e70284716dc74d85cd584ce9fc67
Enrico Zammit <enrico.zammitl@gmail.com>
2020-05-30 12:07:52 +0200

    Move dockerfile due to singularity cwd issues

--- Training/start_running.sh
+++ Training/start_running.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir out/
 export SINGULARITY_DISABLE_CACHE=True
 singularity pull docker://enricozl/gan4ds:argan-runner
-nohup singularity run --nv --bind $(pwd)/out:/Training/out gan4ds_argan-runner.sif > gan4ds.out 2> gan4ds.err < /dev/null &
+nohup singularity run --nv --bind $(pwd)/out:out/ gan4ds_argan-runner.sif > gan4ds.out 2> gan4ds.err < /dev/null &
